daf529c900f463369d46027fd476e849cf97c8f9
cmlee <bal.dongdongdong@gmail.com>
2021-01-11 22:06:37 +0900

    add form module

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
-import { isLoggedInVar } from '../apollo';
+import { FieldErrors, useForm } from 'react-hook-form';
+import { UnpackNestedValue } from 'react-hook-form/dist/types/form';
+
+interface FormFields {
+  email: string;
+  password: string;
+}
 
 function LoggedOutRouter() {
-  const handleLogin = () => {
-    isLoggedInVar(true);
+  const { register, handleSubmit, errors } = useForm<FormFields>();
+
+  const onLogin = (data: UnpackNestedValue<FormFields>) => {
+    console.log('heloo', data);
+  };
+
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
 
   return (
-    <div>
+    <form onSubmit={handleSubmit(onLogin, onInvalid)}>
       <h1>Logged out</h1>
-      <button type="button" onClick={handleLogin}>
-        Click to login
-      </button>
-    </div>
+      <div>
+        <input
+          type="text"
+          name="email"
+          ref={register({
+            required: 'This is required',
+            pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+          })}
+        />
+        {errors.email && (
+          <strong>
+            {errors.email.message}
+            {errors.email.type === 'pattern' && 'Only gmail allowed'}
+          </strong>
+        )}
+      </div>
+      <div>
+        <input
+          type="password"
+          name="password"
+          ref={register({
+            required: 'This is required',
+          })}
+        />
+        {errors.password && <strong>{errors.password.message}</strong>}
+      </div>
+
+      <button>Login</button>
+    </form>
   );
 }
 
