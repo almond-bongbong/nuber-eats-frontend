b19233a866be273c553e94d61f4b1b6e3f02e6c9
cmlee <bal.dongdongdong@gmail.com>
2021-01-14 20:08:20 +0900

    router setting

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,55 +1,20 @@
 import React from 'react';
-import { FieldErrors, useForm } from 'react-hook-form';
-import { UnpackNestedValue } from 'react-hook-form/dist/types/form';
-
-interface FormFields {
-  email: string;
-  password: string;
-}
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from '../pages/login';
+import CreateAccount from '../pages/create-account';
 
 function LoggedOutRouter() {
-  const { register, handleSubmit, errors } = useForm<FormFields>();
-
-  const onLogin = (data: UnpackNestedValue<FormFields>) => {
-    console.log('heloo', data);
-  };
-
-  const onInvalid = (errors: FieldErrors) => {
-    console.log(errors);
-  };
-
   return (
-    <form onSubmit={handleSubmit(onLogin, onInvalid)}>
-      <h1>Logged out</h1>
-      <div>
-        <input
-          type="text"
-          name="email"
-          ref={register({
-            required: 'This is required',
-            pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-          })}
-        />
-        {errors.email && (
-          <strong>
-            {errors.email.message}
-            {errors.email.type === 'pattern' && 'Only gmail allowed'}
-          </strong>
-        )}
-      </div>
-      <div>
-        <input
-          type="password"
-          name="password"
-          ref={register({
-            required: 'This is required',
-          })}
-        />
-        {errors.password && <strong>{errors.password.message}</strong>}
-      </div>
-
-      <button>Login</button>
-    </form>
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
