332d433f86bb9d8e10023614e8db612e8e540634
cmlee <bal.dongdongdong@gmail.com>
2021-01-16 23:41:38 +0900

    update login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import FormError from '../components/form-error';
 import { gql, useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import nuberLogo from '../images/logo.svg';
 import {
   LoginMutation,
   LoginMutationVariables,
 } from '../__generated__/LoginMutation';
+import Button from '../components/button';
 
 interface LoginForm {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 }
 
 const LOGIN_MUTATION = gql`
@@ -23,21 +26,44 @@ const LOGIN_MUTATION = gql`
 `;
 
 export const Login = () => {
-  const { register, getValues, errors, handleSubmit } = useForm<LoginForm>();
-  const [loginMutation] = useMutation<LoginMutation, LoginMutationVariables>(
-    LOGIN_MUTATION
-  );
+  const { register, getValues, errors, handleSubmit, formState } = useForm<
+    LoginForm
+  >({
+    mode: 'onChange',
+  });
+  const [loginMutation, { data, loading }] = useMutation<
+    LoginMutation,
+    LoginMutationVariables
+  >(LOGIN_MUTATION);
 
-  const onSubmit = () => {
-    console.log(getValues());
+  const onSubmit = async () => {
+    if (loading) return;
+
+    const { email, password } = getValues();
+    const { data: loginMutationResult } = await loginMutation({
+      variables: {
+        input: {
+          email,
+          password,
+        },
+      },
+    });
+
+    if (loginMutationResult?.login.ok) {
+      console.log(loginMutationResult.login.token);
+    }
   };
+
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-        <h3 className="text-2xl text-gray-800">Log In</h3>
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={nuberLogo} className="w-52 mb-10" alt="uber eats" />
+        <h4 className="w-full font-medium text-left text-3xl mb-5">
+          Welcome back
+        </h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 mt-5 px-5"
+          className="grid gap-3 mt-5 w-full mb-5"
         >
           <input
             ref={register({ required: 'Email is required' })}
@@ -64,8 +90,19 @@ export const Login = () => {
           {errors.password?.type === 'minLength' && (
             <FormError errorMessage="Password must be more than 10 chars." />
           )}
-          <button className="mt-3 btn">Log In</button>
+          <Button
+            canClick={formState.isValid}
+            loading={loading}
+            actionText={'Log in'}
+          />
+          {data?.login.error && <FormError errorMessage={data.login.error} />}
         </form>
+        <div>
+          New to Nuber?{' '}
+          <Link to="/create-account" className="text-lime-600 hover:underline">
+            Create an Account
+          </Link>
+        </div>
       </div>
     </div>
   );
