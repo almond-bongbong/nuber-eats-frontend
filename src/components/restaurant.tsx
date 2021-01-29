633d95eefb53fdec4fc670247ba58e92db801dd7
cmlee <bal.dongdongdong@gmail.com>
2021-01-30 01:26:49 +0900

    query restaurants

---
+++ src/components/restaurant.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+
+interface Props {
+  id: string;
+  coverImage: string;
+  name: string;
+  categoryName?: string;
+}
+
+function Restaurant({
+  id,
+  coverImage,
+  name,
+  categoryName,
+}: Props): ReactElement {
+  return (
+    <Link to={`/${id}`} className="flex flex-col">
+      <div
+        style={{ backgroundImage: `url(${coverImage})` }}
+        className="bg-red-500 bg-cover bg-center mb-3 py-28"
+      />
+      <h3 className="text-xl">{name}</h3>
+      <span className="border-t mt-4 py-2 text-xs opacity-50 border-gray-300">
+        {categoryName}
+      </span>
+    </Link>
+  );
+}
+
+export default Restaurant;
--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
-import {
-  RestaurantsPageQuery,
-  RestaurantsPageQueryVariables,
-} from '../../__generated__/RestaurantsPageQuery';
+import { RestaurantsPageQuery, RestaurantsPageQueryVariables, } from '../../__generated__/RestaurantsPageQuery';
+import Restaurant from '../../components/restaurant';
 
 const RESTAURANTS_QUERY = gql`
   query RestaurantsPageQuery($input: RestaurantsInput!) {
@@ -38,18 +36,85 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 function Restaurants() {
+  const [page, setPage] = useState(1);
   const { data, loading } = useQuery<
     RestaurantsPageQuery,
     RestaurantsPageQueryVariables
   >(RESTAURANTS_QUERY, {
     variables: {
-      input: { page: 1 },
+      input: { page },
     },
   });
 
   console.log(data);
 
-  return <h1>restaurant</h1>;
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          type="Search"
+          className="input rounded-md border-0 w-3/12"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto ">
+            {data?.allCategories.categories?.map((category) => (
+              <div
+                key={category.id}
+                className="flex flex-col group items-center cursor-pointer"
+              >
+                <div
+                  className="w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full"
+                  style={{ backgroundImage: `url(${category.coverImage})` }}
+                />
+                <span className="mt-1 text-sm text-center font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className="grid mt-16 grid-cols-3 gap-x-5 gap-y-10">
+            {data?.allRestaurants.results?.map((restaurant) => (
+              <Restaurant
+                key={restaurant.id}
+                id={restaurant.id}
+                coverImage={restaurant.coverImage || ''}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={() => setPage((prev) => prev - 1)}
+                className="focus:outline-none font-medium text-2xl"
+              >
+                &larr;
+              </button>
+            ) : (
+              <div />
+            )}
+            <span>
+              Page {page} of {data?.allRestaurants.totalPage}
+            </span>
+            {page !== data?.allRestaurants.totalPage ? (
+              <button
+                onClick={() => setPage((prev) => prev + 1)}
+                className="focus:outline-none font-medium text-2xl"
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div />
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Restaurants;
