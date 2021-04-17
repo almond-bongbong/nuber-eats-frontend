3753fe14ce13da85799de32e90f337094899031a
cmlee <bal.dongdongdong@gmail.com>
2021-04-17 22:50:55 +0900

    create restaurants

--- src/pages/owner/add-restaurant.tsx
+++ src/pages/owner/add-restaurant.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
-import { CreateRestaurantInput } from '../../__generated__/globalTypes';
-import { CreateRestaurantMutationVariables } from '../../__generated__/CreateRestaurantMutation';
+import {
+  CreateRestaurantMutation,
+  CreateRestaurantMutationVariables
+} from '../../__generated__/CreateRestaurantMutation';
 import { useForm } from 'react-hook-form';
 import Button from '../../components/button';
 import { Helmet } from 'react-helmet-async';
+import FormError from "../../components/form-error";
 
 const CREATE_RESTAURANT_MUTATION = gql`
   mutation CreateRestaurantMutation($input: CreateRestaurantInput!) {
@@ -19,29 +22,60 @@ interface CreateRestaurantForm {
   name: string;
   address: string;
   categoryName: string;
+  file: FileList;
 }
 
 function AddRestaurant() {
-  const [createRestaurantMutation, { loading, data }] = useMutation<
-    CreateRestaurantInput,
+  const [uploading, setUploading] = useState(false);
+  const [createRestaurantMutation, { data }] = useMutation<
+    CreateRestaurantMutation,
     CreateRestaurantMutationVariables
   >(CREATE_RESTAURANT_MUTATION);
   const { register, getValues, formState, errors, handleSubmit } = useForm<
     CreateRestaurantForm
   >({ mode: 'onChange' });
 
-  const onSubmit = () => {
-    console.log(getValues());
+  const onSubmit = async () => {
+    const { name, categoryName, address, file } = getValues();
+    const actualFile = file[0];
+    const body = new FormData();
+    body.append('file', actualFile);
+
+    try {
+      setUploading(true);
+      const { url } = await fetch('http://localhost:4001/uploads', {
+        method: 'POST',
+        body,
+      }).then((res) => res.json());
+
+      await createRestaurantMutation({
+        variables: {
+          input: {
+            name,
+            categoryName,
+            address,
+            coverImage: url,
+          },
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setUploading(false);
+    }
   };
 
   return (
-    <div className="container">
+    <div className="container flex flex-col items-center mt-52">
       <Helmet>
         <title>Add Restaurant | Nuber Eats</title>
       </Helmet>
 
-      <h1>Add Restaurant</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <h4 className="font-semibold text-2xl mb-3">Add Restaurant</h4>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5"
+      >
         <input
           type="text"
           name="name"
@@ -63,11 +97,20 @@ function AddRestaurant() {
           className="input"
           ref={register({ required: 'Category Name is required.' })}
         />
+        <div>
+          <input
+            type="file"
+            name="file"
+            accept="image/*"
+            ref={register({ required: true })}
+          />
+        </div>
         <Button
-          loading={loading}
+          loading={uploading}
           canClick={formState.isValid}
           actionText="Create Restaurant"
         />
+        {data?.createRestaurant.error && <FormError errorMessage={data.createRestaurant.error} />}
       </form>
     </div>
   );
