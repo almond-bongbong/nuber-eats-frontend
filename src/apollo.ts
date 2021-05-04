82dbe4e1e265ebcb798926da722809c1b556a72f
cmlee <bal.dongdongdong@gmail.com>
2021-05-04 20:04:20 +0900

    subscription apollo setting

--- src/apollo.ts
+++ src/apollo.ts
@@ -3,14 +3,23 @@ import {
   createHttpLink,
   InMemoryCache,
   makeVar,
+  split,
 } from '@apollo/client';
 import { LOCALSTORAGE_TOKEN_KEY } from './constants';
 import { setContext } from '@apollo/client/link/context';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
-
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4001',
+  options: {
+    reconnect: true,
+  },
+});
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4001/graphql',
@@ -25,8 +34,20 @@ const authLink = setContext((operation, prevContext) => {
   };
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: authLink.concat(splitLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- src/pages/user/order.tsx
+++ src/pages/user/order.tsx
@@ -6,6 +6,7 @@ import {
   GetOrderQuery,
   GetOrderQueryVariables,
 } from '../../__generated__/GetOrderQuery';
+import { Helmet } from "react-helmet-async";
 
 interface Params {
   id: string;
@@ -36,15 +37,55 @@ const GET_ORDER_QUERY = gql`
 
 function Order() {
   const { id } = useParams<Params>();
-  const { data } = useQuery<GetOrderQuery, GetOrderQueryVariables>(GET_ORDER_QUERY, {
-    variables: {
-      input: { id },
-    },
-  });
+  const { data } = useQuery<GetOrderQuery, GetOrderQueryVariables>(
+    GET_ORDER_QUERY,
+    {
+      variables: {
+        input: { id },
+      },
+    }
+  );
 
   console.log(data);
 
-  return <div>Order</div>;
+  return (
+    <div className="mt-32 container flex justify-center">
+      <Helmet>
+        <title>Order #{id}</title>
+      </Helmet>
+      <div className="border border-gray-800 w-full max-w-screen-sm flex flex-col justify-center">
+        <h4 className="bg-gray-800 w-full py-5 text-white text-center text-xl">
+          Order #{id}
+        </h4>
+        <h5 className="p-5 pt-10 text-3xl text-center ">
+          ${data?.getOrder.order?.total}
+        </h5>
+        <div className="p-5 text-xl grid gap-6">
+          <div className="border-t pt-5 border-gray-700">
+            Prepared By:{' '}
+            <span className="font-medium">
+              {data?.getOrder.order?.restaurant?.name}
+            </span>
+          </div>
+          <div className="border-t pt-5 border-gray-700 ">
+            Deliver To:{' '}
+            <span className="font-medium">
+              {data?.getOrder.order?.customer?.email}
+            </span>
+          </div>
+          <div className="border-t border-b py-5 border-gray-700">
+            Driver:{' '}
+            <span className="font-medium">
+              {data?.getOrder.order?.driver?.email || 'Not yet.'}
+            </span>
+          </div>
+          <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">
+            Status: {data?.getOrder.order?.status}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Order;
