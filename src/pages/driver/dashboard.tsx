44871035947de570e2be26307eb225142cbfba66
cmlee <bal.dongdongdong@gmail.com>
2021-05-22 11:03:07 +0900

    panTo update

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,9 +1,17 @@
-import React, { ReactElement, useCallback, useEffect } from 'react';
+import React, { ReactElement, useCallback, useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
+interface Coords {
+  lat: number;
+  lng: number;
+}
+
 function Dashboard(): ReactElement {
+  const [driverCoords, setDriverCoords] = useState<Coords>({ lat: 0, lng: 0 });
+
   const onSuccess: PositionCallback = useCallback((position) => {
-    console.log(position);
+    const { latitude, longitude } = position.coords;
+    setDriverCoords({ lat: latitude, lng: longitude });
   }, []);
 
   const onError: PositionErrorCallback = useCallback((error) => {
@@ -11,10 +19,35 @@ function Dashboard(): ReactElement {
   }, []);
 
   useEffect(() => {
-    navigator.geolocation.watchPosition(onSuccess, onError, {
+    const watchId = navigator.geolocation.watchPosition(onSuccess, onError, {
       enableHighAccuracy: true,
     });
+
+    return () => {
+      navigator.geolocation.clearWatch(watchId);
+    };
   }, [onSuccess, onError]);
+
+  const onApiLoaded = ({
+    map,
+    maps,
+  }: {
+    map: google.maps.Map;
+    maps: {
+      LatLng: {
+        new (
+          latOrLatLngLiteral: number | google.maps.LatLngLiteral,
+          lngOrNoWrap?: number | boolean | null,
+          noWrap?: boolean
+        ): google.maps.LatLng;
+      };
+    };
+    ref: Element | null;
+  }) => {
+    setTimeout(() => {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }, 2000);
+  };
 
   return (
     <div>
@@ -23,17 +56,17 @@ function Dashboard(): ReactElement {
         style={{ width: window.innerWidth, height: '95vh' }}
       >
         <GoogleMapReact
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
           bootstrapURLKeys={{
             key: process.env.REACT_APP_GOOGLE_MAP_API_KEY || '',
           }}
-          defaultZoom={11}
+          defaultZoom={15}
           defaultCenter={{
-            lat: 59.95,
-            lng: 30.33,
+            lat: 36.58,
+            lng: 125.95,
           }}
-        >
-          <h1>hello</h1>
-        </GoogleMapReact>
+        />
       </div>
     </div>
   );
